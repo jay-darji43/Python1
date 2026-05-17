5b1db6f60031ac62e2741412270a377ce083f8e8
jay-darji43 <jaydarji2456@gmail.com>
2026-05-17 09:11:35 +0530

    Add files via upload

---
+++ loop.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    //int n;
+    //printf("enter the value");
+    //scanf("%d",&n);
+    for(int i=1;i<=10;i=i+2){
+    printf("man jain\n "); 
+    }
+    return 0;
+}
